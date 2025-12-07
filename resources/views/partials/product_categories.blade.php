839ae29041fb7abc378b5e0a61cde403f9730a88
HK-MBURU <kiarii.mburu20@students.dkut.ac.ke>
2025-12-07 07:07:53 +0300

    Category Addingq3

--- resources/views/partials/product_categories.blade.php
+++ resources/views/partials/product_categories.blade.php
@@ -18,7 +18,7 @@
                             height="200">
                 @endif
                 <h3>{{ $category->name }}</h3>
-                <p>{{ Str::limit($category->description, 100) }}</p> {{-- MODIFIED LINE --}}
+                <p>{{ Str::limit($category->description, 120) }}</p> {{-- MODIFIED LINE --}}
                 <a href="{{ route('products.category', $category->id) }}" class="btn-secondary text-center">View Details</a>
             </div>
         @endforeach
