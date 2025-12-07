c932aa2f3ebff4bdf5a4a528f43d0695a8dde7ba
HK-MBURU <kiarii.mburu20@students.dkut.ac.ke>
2025-12-07 07:05:44 +0300

    Category Addingq

--- resources/views/partials/product_categories.blade.php
+++ resources/views/partials/product_categories.blade.php
@@ -5,20 +5,20 @@
             <div class="product-card">
                 @if ($category->image_url)
                     <img src="{{ $category->image_url }}"
-                         alt="{{ $category->name }}"
-                         loading="lazy"
-                         width="300"
-                         height="200">
+                            alt="{{ $category->name }}"
+                            loading="lazy"
+                            width="300"
+                            height="200">
                 @else
                     <img src="{{ asset('images/no-image-available.jpg') }}"
-                         alt="No image available"
-                         class="img-fluid"
-                         loading="lazy"
-                         width="300"
-                         height="200">
+                            alt="No image available"
+                            class="img-fluid"
+                            loading="lazy"
+                            width="300"
+                            height="200">
                 @endif
                 <h3>{{ $category->name }}</h3>
-                <p>{{ $category->description }}</p>
+                <p>{{ Str::limit($category->description, 50) }}</p> {{-- MODIFIED LINE --}}
                 <a href="{{ route('products.category', $category->id) }}" class="btn-secondary text-center">View Details</a>
             </div>
         @endforeach
